1c06f2c8a9be59af7a24387c0c9a79130ed86acc
Crowdbotics <team@crowdbotics.com>
2024-02-14 23:11:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cliniquick-46366",
+  url: "https://cliniquick-46366.botics.co",
+  api: "https://cliniquick-46366.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
